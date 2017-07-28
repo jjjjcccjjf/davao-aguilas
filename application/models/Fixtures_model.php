49a2e2a4a61fa1ad497a36cb8c81295b5b487636
Lorenzo Salamante <lsalamante@myoptimind.com>
2017-07-28 17:25:42 +0800

    Add get upcoming matches function

--- application/models/Fixtures_model.php
+++ application/models/Fixtures_model.php
@@ -39,13 +39,18 @@ class Fixtures_model extends Crud_model
   }
 
   function getUpcoming(){
-
+    $fixtures = $this->db->query("SELECT * FROM `fixtures` WHERE `match_schedule` >= NOW() AND `match_progress` = 'Upcoming' ORDER BY `match_schedule` LIMIT 1")->result();
+    $this->formatFields($fixtures);
+    $res['matches'] = $fixtures;
+    $res['league_name'] = $this->getLeagueName($fixtures[0]->league_id);
+    return $res;
   }
 
   function getRecent(){
     $this->db->where('match_progress', 'final');
     $this->db->order_by('match_schedule', 'DESC');
-    $fixtures = array_slice($this->db->get($this->table)->result(), 0, 1);
+    $this->db->limit(1);
+    $fixtures = $this->db->get($this->table)->result();
     $this->formatFields($fixtures);
 
     $res['matches'] = $fixtures;
