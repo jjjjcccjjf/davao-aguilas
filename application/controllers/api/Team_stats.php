41a83ff797dba1f7d806ee5531c5446b7636a823
Lorenzo Salamante <lsalamante@myoptimind.com>
2017-08-01 15:04:39 +0800

    Add team statistics for CMS

--- application/controllers/api/Team_stats.php
+++ application/controllers/api/Team_stats.php
@@ -10,8 +10,19 @@ class Team_stats extends Crud_controller
 
   }
 
-  function team_get($team_id){
-    $res = $this->model->getStatsByTeamId($team_id);
+  /**
+   * [team_get description]
+   * @param  [type] $team_id [description]
+   * @param  [type] $param   'default' means return davao's default team stats
+   * @return [type]          [description]
+   */
+   function team_get($team_id, $param = null){
+
+    if($param == 'default'){
+      $res = $this->model->getDefaultTeamStats();
+    }else{
+      $res = $this->model->getStatsByTeamId($team_id);
+    }
 
     if($res || $res !== []){ # Respond with 404 when the resource is not found
       $this->response($res, 200);
--- application/models/Team_stats_model.php
+++ application/models/Team_stats_model.php
@@ -16,9 +16,7 @@ class Team_stats_model extends Crud_model
     $res = $this->db->get($this->table)->result();
 
     /* we strip off useless zeros such as 60.00 to 60 or 79.70 to 79.9 */
-    foreach($res as &$val) {
-      $val->stat_value = floatval($val->stat_value);
-    }
+    $this->formatFields($res);
 
     return $res;
   }
@@ -28,11 +26,26 @@ class Team_stats_model extends Crud_model
     $this->db->where('team_id', $team_id);
     $res = $this->db->get($this->table)->result();
 
+    $this->formatFields($res);
+
+    return $res;
+  }
+
+  public function getDefaultTeamStats()
+  {
+    $this->load->model('Players_model');
+    $default_team_id = $this->Players_model->getDefaultSquad()[0]->team_id; # Get a single player from a squad then get his ID
+    $res = $this->getStatsByTeamId($default_team_id);
+    $this->formatFields($res);
+
+    return $res;
+  }
+
+  public function formatFields($res)
+  {
     foreach($res as &$val) {
       $val->stat_value = floatval($val->stat_value);
     }
-
-    return $res;
   }
 
 }
