99f0bfa05bb71062c13de68bc476400a3b1a4765
Lorenzo Salamante <lsalamante@myoptimind.com>
2017-08-11 17:44:29 +0800

    Fix match_stats bug fixtures

--- application/controllers/api/Fixtures.php
+++ application/controllers/api/Fixtures.php
@@ -37,11 +37,29 @@ class Fixtures extends Crud_controller
       $this->response(['message' => 'Not found'], 404);
     }
   }
+
   function recent_get(){
 
     $res = $this->model->getRecent();
 
     if($res['matches'] !== [] && $res['league_name'] !== null){ # Respond with 404 when the resource is not found
+      $this->response($res, 200);
+    }else{
+      $this->response(['message' => 'Not found'], 404);
+    }
+  }
+
+  function single_get($id)
+  {
+    $res = $this->model->get($id);
+
+
+    if($res || $res !== []){ # Respond with 404 when the resource is not found
+
+      # For making match_stats never missing key
+      if(!array_key_exists('match_stats', $res[0]))
+      $res[0]->match_stats = [];
+
       $this->response($res, 200);
     }else{
       $this->response(['message' => 'Not found'], 404);
